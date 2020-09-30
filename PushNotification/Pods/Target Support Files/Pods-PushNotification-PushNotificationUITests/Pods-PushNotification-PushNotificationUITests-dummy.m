0fe6824b9f5fa9027bc4e660d51c227c69994707
LuyenDao <luyenddgc00774@gmail.com>
2020-09-30 09:51:13 +0700

    push notification ( Chưa xong) Lỗi Provisioning profile "iOS Team Provisioning Profile: DaoDinhLuyen.PushNotification" doesn't support the Push Notifications capability. tạo fireBase lấy identifier import fireBase config appdelegate

---
+++ PushNotification/Pods/Target Support Files/Pods-PushNotification-PushNotificationUITests/Pods-PushNotification-PushNotificationUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PushNotification_PushNotificationUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PushNotification_PushNotificationUITests
+@end
